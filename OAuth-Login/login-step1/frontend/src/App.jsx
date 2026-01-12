224e5d691ca83cc84c511a3031a540a24e01c471
사과 <sbd2953@gmail.com>
2026-01-12 17:19:34 +0900

    구글클라이언트ID발급 및 리다이렉트 - 7교시

---
+++ OAuth-Login/login-step1/frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import "bootstrap/dist/css/bootstrap.min.css"
+import { Route, Routes } from "react-router-dom"
+import HomePage from "./components/pages/HomePage"
+import LoginView from "./components/auth/LoginView"
+
+function App() {
+
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />}/>
+        <Route path="/login" element={<LoginView />}/>
+        <Route path="/oauth/google/redirect" element={<GoogleRedirect />} />
+      </Routes>
+    </>  
+  )
+}
+
+export default App
